712c56be01d2cb2ab869fd9ded6631cca14b282b
cHarb345 <cHarb345@gmail.com>
2020-02-14 20:59:29 -0500

    subsystems

--- src/main/java/frc/robot/subsystems/Extender.java
+++ src/main/java/frc/robot/subsystems/Extender.java
@@ -9,7 +9,10 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Extender extends SubsystemBase {
@@ -18,15 +21,18 @@ public class Extender extends SubsystemBase {
    */
   private final CANSparkMax leftSpin;
   private final CANSparkMax rightSpin;
+  private final WPI_TalonSRX intake;
 
   public Extender() {
     leftSpin = new CANSparkMax(22, MotorType.kBrushless);
     rightSpin = new CANSparkMax(0, MotorType.kBrushless);
+    intake = new WPI_TalonSRX(0);
   }
 
   public void extend() {
     leftSpin.set(0.5);
     rightSpin.set(0.5);
+    intake.set(0.8);
   }
 
   @Override
